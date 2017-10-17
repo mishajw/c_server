454de96b1f9fccde8abdb08c730275be5008c18e
Misha Wagner <mishajw@gmail.com>
2017-10-17 09:51:21 +0100

    Add function comments to server.h

--- src/server.h
+++ src/server.h
@@ -1,15 +1,22 @@
 #include <inttypes.h>
 #include <stddef.h>
 
+// Server being used to serve clients
 struct server;
+
+// A connection between a server and a single client
 struct connection;
 
+// Initialise a server on the port `port_number`
 struct server* create_server(uint16_t port_number);
 
+// Wait for a connection to be made the server, and return the connection struct for the new connection
 struct connection* create_connection(struct server *server);
 
+// Get the next message from a connection
 size_t get_message(struct connection *connection, char *message, size_t message_size);
 
+// Deallocate resources used by the server
 void destroy_server(struct server *server);
 
 // Deallocate resources used by the connection
